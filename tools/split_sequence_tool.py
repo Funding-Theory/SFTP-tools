1ef5b35c219e06e4aa163e0af76c80fd39c6bb57
Animesh <animesh.workplace@gmail.com>
2021-06-09 16:34:21 +0530

    optimized the code for very large fasta files

--- tools/split_sequence_tool.py
+++ tools/split_sequence_tool.py
@@ -11,19 +11,8 @@ sequence_url = args.fasta
 metadata_url = args.selected
 
 selected = pandas.read_csv(metadata_url, delimiter = '\t', encoding = 'utf-8')
-sequence = SeqIO.to_dict(SeqIO.parse(sequence_url, 'fasta'))
-adjusted_sequence = []
-ids_not_present = []
+selected_sequence = []
 
-for i in tqdm(selected['ids']):
-	if(i in list(sequence.keys())):
-		adjusted_sequence.append(sequence[i])
-	else:
-		ids_not_present.append(i)
+test = [selected_sequence.append(i) for i in tqdm(SeqIO.parse(sequence_url, 'fasta')) if(i.id in selected['ids'])]
 
-if(len(ids_not_present)):
-	print('Following ids were not present in the fasta file provided')
-	for i in ids_not_present:
-		print(i)
-
-SeqIO.write(adjusted_sequence, 'selected_sequences.fasta', 'fasta')
+SeqIO.write(selected_sequence, 'selected_sequences.fasta', 'fasta')
