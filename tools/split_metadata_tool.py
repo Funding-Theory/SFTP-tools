c5956b7aa0301a305e030f814b9a0ba5daa9f868
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-28 15:03:47 +0530

    added new tool for splitting metadata

---
+++ tools/split_metadata_tool.py
@@ -0,0 +1,30 @@
+import numpy
+import pandas
+import argparse
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--search', help='Enter Search File with header as ids')
+parser.add_argument('--metadata', help='Enter NIBMG Datahub metadata')
+args = parser.parse_args()
+search_url = args.search
+metadata_url = args.metadata
+
+search = pandas.read_csv(search_url, delimiter = '\t', encoding = 'utf-8', low_memory = False)
+metadata = pandas.read_csv(metadata_url, delimiter = '\t', encoding = 'utf-8', low_memory = False)
+matched = []
+unmatched = []
+multimatch = []
+
+for i in tqdm(search['ids']):
+	match = [index for (index, j) in enumerate(metadata['strain'].tolist()) if(i in j)]
+	if(len(match) == 0):
+		unmatched.append(i)
+	elif(len(match) == 1):
+		matched.append(match[0])
+	else:
+		multimatch.append(i)
+
+pandas.DataFrame(unmatched, columns = ['All Unmatched']).to_csv('unmatched.tsv', index = False, sep = '\t')
+pandas.DataFrame(multimatch, columns = ['All Multimatch']).to_csv('multi_matched.tsv', index = False, sep = '\t')
+metadata.iloc[matched].to_csv('matched.tsv', index = False, sep = '\t') 
